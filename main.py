c52788137fb319ac7ca769ca010290a614ca7d14
shadowik <zahar.mark@bk.ru>
2021-02-03 20:55:46 +0400

    NORMAL IMG

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ class Church(pygame.sprite.Sprite):
         self.percent = 50
         self.image = pygame.transform.scale(load_image('char/church/0%.png'), (100, 100))
         self.rect = self.image.get_rect()
-        self.rect.x = ((WIDTH - 500 - WIDTH // 10) * 0.5 - 20) * 1.2
+        self.rect.x = ((WIDTH - 500 - WIDTH // 10) * 0.5 - 20) * 1.3
         self.rect.y = int(HEIGHT * 0.02)
 
 
@@ -28,10 +28,10 @@ class Social(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__(all_sprites)
         self.percent = 50
-        self.image = pygame.transform.scale(load_image('char/social/0%.png'), (125, 110))
+        self.image = pygame.transform.scale(load_image('char/social/0%.png'), (100, 100))
         self.rect = self.image.get_rect()
-        self.rect.x = ((WIDTH - 500 - WIDTH // 10) * 0.5 - 20) * 1.95
-        self.rect.y = int(HEIGHT * 0.02) - 5
+        self.rect.x = ((WIDTH - 500 - WIDTH // 10) * 0.5 - 20) * 2
+        self.rect.y = int(HEIGHT * 0.02)
 
 
 class Army(pygame.sprite.Sprite):
@@ -40,18 +40,18 @@ class Army(pygame.sprite.Sprite):
         self.percent = 50
         self.image = pygame.transform.scale(load_image('char/army/0%.png'), (100, 100))
         self.rect = self.image.get_rect()
-        self.rect.x = ((WIDTH - 500 - WIDTH // 10) * 0.5 - 20) * 2.7
-        self.rect.y = int(HEIGHT * 0.02) - 5
+        self.rect.x = ((WIDTH - 500 - WIDTH // 10) * 0.5 - 20) * 2.9
+        self.rect.y = int(HEIGHT * 0.02)
 
 
-"""class Money(pygame.sprite.Sprite):
+class Money(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__(all_sprites)
         self.percent = 50
-        self.image = pygame.transform.scale(load_image('char/army/0%.png'), (100, 100))
+        self.image = pygame.transform.scale(load_image('char/money/0%.png'), (100, 100))
         self.rect = self.image.get_rect()
-        self.rect.x = ((WIDTH - 500 - WIDTH // 10) * 0.5 - 20) * 2.7
-        self.rect.y = int(HEIGHT * 0.02) - 5"""
+        self.rect.x = ((WIDTH - 500 - WIDTH // 10) * 0.5 - 20) * 3.6
+        self.rect.y = int(HEIGHT * 0.02)
 
 
 class Card(pygame.sprite.Sprite):
@@ -67,7 +67,9 @@ class Card(pygame.sprite.Sprite):
         global LAST_POS, diff_with_card
         if LAST_POS:
             x = LAST_POS[0] - pygame.mouse.get_pos()[0]
-            if pygame.mouse.get_pos()[0] not in range(int((WIDTH - 500 - WIDTH // 10) * 0.5), int((WIDTH - 500 - WIDTH // 10) * 0.5 + 500 + WIDTH // 10) + 1):
+            if pygame.mouse.get_pos()[0] not in range(int((WIDTH - 500 - WIDTH // 10) * 0.5),
+                                                      int((WIDTH - 500 - WIDTH // 10) * 0.5
+                                                          + 500 + WIDTH // 10) + 1):
                 return
             self.rect.x -= x
             LAST_POS = (pygame.mouse.get_pos()[0], LAST_POS[1])
@@ -81,6 +83,7 @@ class Card(pygame.sprite.Sprite):
 church = Church()
 social = Social()
 army = Army()
+money = Money()
 card = Card()
 
 
